6702c2e8166a767e2705f9c6b2f0bc25df265f4d
Callon Campbell <calloncampbell@msn.com>
2017-04-06 23:54:29 -0400

    Minor assembly info change.

--- assets/VersionInfo.cs
+++ assets/VersionInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("ReflectSoftware Inc.")]
 [assembly: AssemblyProduct("ReflectSoftware.Insight.Extensions Library")]
-[assembly: AssemblyCopyright("© 2016 ReflectSoftware Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("© 2017 ReflectSoftware Inc. All rights reserved.")]
 [assembly: AssemblyDescription("Provides extensions to the ReflectInsight Logging Framework")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
